1426d209a2e8c0ecee77384cc3ff72d5327435d2
Adam Smith <adamsmith@Adams-iMac.local>
2014-12-22 22:10:21 +0000

    fixed some indentation issues

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-import user from '../models/user';
+// import user from '../models/user';
 
 export default Ember.ObjectController.extend({
 
@@ -10,29 +10,29 @@ export default Ember.ObjectController.extend({
   //
    
   actions: {
-  	  videos: function() {
-  	  	this.send('closeMenu');
-  	  	this.transitionToRoute('videos', {queryParams: {state: null, code: null}});
-  	  },
-  	  about: function() {
-  	  	this.send('closeMenu');
-  	  	this.transitionToRoute('about', {queryParams: {state: null, code: null}});
-  	  },
-  	  login: function() {
-  	  	this.send('closeMenu');
-  	  	this.transitionToRoute('login', {queryParams: {state: null, code: null}});
-  	  },
-	  closeMenu: function() {
-	  	Ember.$('body').removeClass('open');
-	  	Ember.$('.app-bar').removeClass('open');
-	  	Ember.$('.navdrawer-container').removeClass('open');
-	  	Ember.$('.navdrawer-container').addClass('opened');
-	  },
-	  toggleMenu: function () {
-	  	Ember.$('body').toggleClass('open');
-	  	Ember.$('.app-bar').toggleClass('open');
-	  	Ember.$('.navdrawer-container').toggleClass('open');
-	  	Ember.$('.navdrawer-container').addClass('opened');
-	  },
+    videos: function() {
+      this.send('closeMenu');
+      this.transitionToRoute('videos', {queryParams: {state: null, code: null}});
+  	},
+  	about: function() {
+      this.send('closeMenu');
+      this.transitionToRoute('about', {queryParams: {state: null, code: null}});
+  	},
+    login: function() {
+      this.send('closeMenu');
+      this.transitionToRoute('login', {queryParams: {state: null, code: null}});
+  	},
+    closeMenu: function() {
+    	Ember.$('body').removeClass('open');
+    	Ember.$('.app-bar').removeClass('open');
+    	Ember.$('.navdrawer-container').removeClass('open');
+    	Ember.$('.navdrawer-container').addClass('opened');
+    },
+    toggleMenu: function () {
+    	Ember.$('body').toggleClass('open');
+    	Ember.$('.app-bar').toggleClass('open');
+    	Ember.$('.navdrawer-container').toggleClass('open');
+    	Ember.$('.navdrawer-container').addClass('opened');
+    },
   }
 });
--- app/controllers/oauth.js
+++ app/controllers/oauth.js
@@ -14,25 +14,25 @@ export default Ember.ObjectController.extend({
 
   //https://github.com/reddit/reddit/wiki/OAuth2#token-retrieval-implicit-grant-flow
   login: function() {
-  	var options = this.authorizationModel();
+    var options = this.authorizationModel();
     var url = "https://ssl.reddit.com/api/v1/authorize?client_id="+options.client_id+"&response_type="+options.response_type+"&state="+options.init_state+"&redirect_uri="+options.redirect_uri+"&duration="+options.duration+"&scope="+options.scope;
     window.location.assign(url);
   },
 
   logout: function() {
-  	var _this = this;
+    var _this = this;
     this.revokeAccessToken().then(function() {
       user.clear();
-	  _this.transitionToRoute('videos');
+      _this.transitionToRoute('videos');
     });
-   },
+  },
 
-   clearAuthUrl: function() {
-     window.history.replaceState( {} , '', '/' );
-   },
+  clearAuthUrl: function() {
+    window.history.replaceState( {} , '', '/' );
+  },
 
-   //https://www.reddit.com/dev/api#GET_api_v1_me
-   getUserInfo: function() {
+  //https://www.reddit.com/dev/api#GET_api_v1_me
+  getUserInfo: function() {
     var _this = this;	
 
     return Ember.$.ajax({
@@ -57,7 +57,6 @@ export default Ember.ObjectController.extend({
       	alert('Your session has expired');
       	_this.login();
       } else {
-      	debugger;
       	alert('Error getting user details');
       }
     });
@@ -67,81 +66,81 @@ export default Ember.ObjectController.extend({
     var text = "";
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     for (var i=0; i < len; i++ ) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
     }
     return text;
    },
 
-   authorizationModel: function() {
-	  return {
-		 client_id: 'NjCSqf0hIl2emQ',
-		 init_state: this.makeid(5),
-		 response_type: 'code',
-		 duration: 'temporary',
-		 redirect_uri: 'http://' + window.location.host,
-		 scope: ['modposts',
-			'identity',
-			'edit',
-			'flair',
-			'history',
-			'modconfig',
-			'modflair',
-			'modlog',
-			'modposts', 
-			'modwiki',
-			'mysubreddits',
-			'privatemessages',
-			'read',
-			'report', 
-			'save',
-			'submit',
-			'subscribe',
-			'vote',
-			'wikiedit',
-			'wikiread']
-	 };
+  authorizationModel: function() {
+    return {
+      client_id: 'NjCSqf0hIl2emQ',
+      init_state: this.makeid(5),
+      response_type: 'code',
+      duration: 'temporary',
+      redirect_uri: 'http://' + window.location.host,
+      scope: ['modposts',
+      'identity',
+      'edit',
+      'flair',
+      'history',
+      'modconfig',
+      'modflair',
+      'modlog',
+      'modposts', 
+      'modwiki',
+      'mysubreddits',
+      'privatemessages',
+      'read',
+      'report', 
+      'save',
+      'submit',
+      'subscribe',
+      'vote',
+      'wikiedit',
+      'wikiread']
+    };
    },
 
-   //https://github.com/reddit/reddit/wiki/OAuth2#manually-revoking-a-token
-   revokeAccessToken: function() {
-   	var url = '/ssl-reddit/api/v1/revoke_token';
+  //https://github.com/reddit/reddit/wiki/OAuth2#manually-revoking-a-token
+  revokeAccessToken: function() {
+    var url = '/ssl-reddit/api/v1/revoke_token';
 
-	return Ember.$.ajax({
-	  type: "POST",
-	  url: url,
-	  headers: {
-	    "Authorization": "Basic " + btoa(config.APP.REDDIT.client_id + ":" + ''),
-	  },	  
-	  data: { 
-	  	token: user.access_token,
-	  },
-	}).fail(function() {
-	  alert('Error trying to revoke token');
-	});   	 
-   },
+    return Ember.$.ajax({
+      type: "POST",
+      url: url,
+      headers: {
+        "Authorization": "Basic " + btoa(config.APP.REDDIT.client_id + ":" + ''),
+      },	  
+      data: { 
+        token: user.access_token,
+      },
+    }).fail(function() {
+      alert('Error trying to revoke token');
+    });   	 
+  },
 
    //https://github.com/reddit/reddit/wiki/OAuth2#token-retrieval-code-flow
-   getAccessToken: function(params) {
+  getAccessToken: function(params) {
    	var _this = this;
-	var url = '/ssl-reddit/api/v1/access_token';
+    var url = '/ssl-reddit/api/v1/access_token';
 
-	return Ember.$.ajax({
-	  type: "POST",
-	  url: url,
-	  headers: {
-	    "Authorization": "Basic " + btoa(config.APP.REDDIT.client_id + ":" + ''),
-	  },
-	  data: { 
-	    grant_type: 'authorization_code', 
-	    code: params.code,
-	    redirect_uri: 'http://' + window.location.host,
-	    state: params.state 
-	  },
-	}).then(function(data) {
-	  user.access_token = data.access_token;
-	  return _this.getUserInfo();
-	}).fail(function(){
-	  alert('Error trying to gain access token');
-	});
-   }
+    return Ember.$.ajax({
+      type: "POST",
+	    url: url,
+	    headers: {
+        "Authorization": "Basic " + btoa(config.APP.REDDIT.client_id + ":" + ''),
+      },
+	    data: { 
+        grant_type: 'authorization_code', 
+        code: params.code,
+        redirect_uri: 'http://' + window.location.host,
+        state: params.state 
+      },
+    }).then(function(data) {
+      user.access_token = data.access_token;
+      return _this.getUserInfo();
+    }).fail(function() {
+      alert('Error trying to gain access token');
+    });
+  }
 });
