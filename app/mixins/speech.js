6599bc75998979cc55d1696ff561816e9f54683e
Adam Smith <abs2g08@gmail.com>
2014-12-29 13:52:10 +0000

    add missing file

---
+++ app/mixins/speech.js
@@ -0,0 +1,31 @@
+import Ember from 'ember';
+import SpeechRouter from 'vendor/speech-router';
+
+export default Ember.Mixin.create({
+	_speechRouter: null,
+	_speechRouterOn: true,
+	toggleSpeechRouter: function() {
+		if(this._speechRouterOn) {
+        	this.speechRouter.stop();
+		} else {
+        	this.speechRouter.start();			
+		}
+		this._speechRouterOn = !this._speechRouterOn;
+		return this._speechRouterOn;
+	},
+	init: function() {
+		var _this = this;
+		var names = this.router.router.recognizer.names;
+		var routes = { routes: {} };
+		Ember.$.each(names, function(key) {
+			routes.routes[("transition " + key)] = key;
+			routes[key] = function() {
+				_this.transitionTo(key);
+			};
+		});
+
+		this.speechRouter = new SpeechRouter(routes);
+        this.speechRouter.start();
+        this._super();
+	}
+});
